ca13ad0375426fb091c22cee2c59a77f3e287284
Feruz <farxodovferuz33@gmail.com>
2023-08-17 08:10:19 -0400

    Final commit

--- src/main/java/org/example/spring/model/Todo.java
+++ src/main/java/org/example/spring/model/Todo.java
@@ -8,10 +8,11 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Getter
 @Setter
+@Builder
 @ToString
 public class Todo {
     private Integer id;
     private String title;
     private String priority;
-    private LocalDateTime createdAt;
+    private Object createdAt;
 }
